c7d16e80a66200d641bd31b2e0d582066a4a016d
Javier Hinojosa Muñoz <javi161007@gmail.com>
2026-03-16 12:00:24 +0100

    update methods

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -16,9 +16,15 @@ import java.text.SimpleDateFormat;
  * month, and year values cannot be changed.
  */
 public class Date {
+  private int fieldAddedByCollaborator;
   private final int day;
   private final int month;
   private final int year;
+
+  public void methodAddedByOwner() {
+    System.out.println("Method added by the owner");
+  }
+
 
   /**
    * Retrieves the day component of this date.
